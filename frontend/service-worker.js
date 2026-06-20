bfa7a109e21afabba2ad9eaed862b15672a6b078
anggunagistina1-gif <anggunagistina1@gmail.com>
2026-06-20 13:45:44 +0700

    Complete PWA setup

---
+++ frontend/service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "profil-pwa-v1";
+
+const urlsToCache = [
+    "./",
+    "./profil.html",
+    "./login.html",
+    "./cms.html",
+    "./manifest.json",
+    "./asset/css/style.css"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
